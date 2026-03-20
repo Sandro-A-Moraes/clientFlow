2a1cf484e5c82a91d604699aa518418301cbc437
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-03-20 18:37:29 -0300

    feat: implement UserRepository with methods for finding and creating users

---
+++ src/modules/user/repository/user.repository.ts
@@ -0,0 +1,30 @@
+import { prisma } from "../../../lib/prisma";
+
+export class UserRepository {
+  public async findByEmail(email: string) {
+    return await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+  }
+
+  public async findById(id: string) {
+    return await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  public async create(data: { name: string; email: string; password: string }) {
+    return await prisma.user.create({
+      data,
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+  }
+}
