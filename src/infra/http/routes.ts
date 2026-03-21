6fbdd7bd6192e585f6c2d599a43f9b3474cafe9d
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-03-20 21:49:12 -0300

    feat: add authentication routes to the main router

--- src/infra/http/routes.ts
+++ src/infra/http/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { authRoutes } from "../../modules/auth/routes/auth.routes";
 
 
 const router = Router();
 
+router.use("/auth", authRoutes);
 
 router.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
+
 
 export default router;
