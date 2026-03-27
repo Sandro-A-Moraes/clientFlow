1e8fc2ed8ffa18838b197c6f4d3ef4f5be176e40
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-03-27 16:05:12 -0300

    refactor: improve appointment route formatting for better readability

--- src/modules/appointments/routes/appointment.routes.ts
+++ src/modules/appointments/routes/appointment.routes.ts
@@ -5,16 +5,27 @@ import { AppointmentRepository } from "../repository/appointment.repository";
 import { AuthMiddleware } from "../../auth/middleware/auth.middleware";
 import { ClientRepository } from "../../clients/repository/client.repository";
 
-
 const appointmentRepository = new AppointmentRepository();
 const clientRepository = new ClientRepository();
-const appointmentService = new AppointmentService(appointmentRepository, clientRepository);
+const appointmentService = new AppointmentService(
+  appointmentRepository,
+  clientRepository,
+);
 const appointmentController = new AppointmentController(appointmentService);
 const authMiddleware = new AuthMiddleware();
 
 const appointmentRouter = Router();
 
-appointmentRouter.post("/", authMiddleware.authenticate, appointmentController.create);
-appointmentRouter.get("/:clientId", authMiddleware.authenticate, appointmentController.listByClientId);
+appointmentRouter.post(
+  "/",
+  authMiddleware.authenticate,
+  appointmentController.create,
+);
+
+appointmentRouter.get(
+  "/appointments?clientId=abc",
+  authMiddleware.authenticate,
+  appointmentController.listByClientId,
+);
 
 export { appointmentRouter };
