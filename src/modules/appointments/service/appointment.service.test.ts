5dde185b382d815a47381c32434611daea4ef854
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-03-30 11:34:08 -0300

    feat: add expect verifications

--- src/modules/appointments/service/appointment.service.test.ts
+++ src/modules/appointments/service/appointment.service.test.ts
@@ -1,32 +1,43 @@
 import { describe, it, expect, vi } from "vitest";
 import { AppointmentService } from "./appointment.service";
 
-describe('AppointmentService', () => { 
-    it("should create appointment when data is valid", async()=>{
-        const clientRepository = {
-            findByIdAndUserId: vi.fn().mockResolvedValue({id:"cliente-1"})
-        }
+describe("AppointmentService", () => {
+  it("should create appointment when data is valid", async () => {
+    const clientRepository = {
+      findByIdAndUserId: vi.fn().mockResolvedValue({ id: "cliente-1" }),
+    };
 
-        const appointmentRepository = {
-            create: vi.fn().mockResolvedValue({id: "1"})
-        }
+    const appointmentRepository = {
+      create: vi.fn().mockResolvedValue({ id: "1" }),
+    };
 
-        const service = new AppointmentService(
-            appointmentRepository as any,
-            clientRepository as any
-        )
+    const service = new AppointmentService(
+      appointmentRepository as any,
+      clientRepository as any,
+    );
 
-        const input = {
-          userId: "user-1",
-          clientId: "client-1",
-          description: "Consulta",
-          scheduledAt: "2026-03-30T10:00:00.000Z",
-          status: "pending",
-        };
+    const input = {
+      userId: "user-1",
+      clientId: "client-1",
+      description: "Consulta",
+      scheduledAt: "2026-03-30T10:00:00.000Z",
+      status: "pending",
+    };
 
-        const result = await service.create(input);
+    const result = await service.create(input);
 
-        expect(result).toEqual({ id: "1" });
-        expect(service.create).toHaveBeenCalled();
+    expect(result).toEqual({ id: "1" });
+
+    expect(clientRepository.findByIdAndUserId).toHaveBeenCalledWith(
+      "client-1",
+      "user-1",
+    );
+
+    expect(appointmentRepository.create).toHaveBeenCalledWith({
+      clientId: "client-1",
+      description: "Consulta",
+      scheduledAt: expect.any(Date),
+      status: "pending",
     });
- })
+  });
+});
