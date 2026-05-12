1c7c52ece3002e288b75e725f0b18211f36e3e66
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-05-12 12:59:02 -0300

    feat: implement rate limiting middleware using Redis for authentication

---
+++ src/infra/http/middleware/security/auth-rate-limit.ts
@@ -0,0 +1,19 @@
+import rateLimit from "express-rate-limit";
+import { RedisStore } from "rate-limit-redis";
+import { redisClient } from "./redis.js";
+
+export const authRateLimit = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+
+  standardHeaders: true,
+  legacyHeaders: false,
+
+  message: {
+    error: "Too many login attempts",
+  },
+
+  store: new RedisStore({
+    sendCommand: (...args: string[]) => redisClient.sendCommand(args),
+  }),
+});
